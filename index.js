6bb13ad63c95202e7a47fca1acbb63c42b969696
Dong Xia <dong7777xia@gmail.com>
2020-09-13 23:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  newtext();
+});
+
+function newtext() {
+  let text = document".querySelector("p#text")
+  text.textContent = "This is really cool!"
+}
